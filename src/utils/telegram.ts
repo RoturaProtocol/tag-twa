1d5b29d6859ed6982f079b32d187c47ba06d428f
Cyka <1354250064mdzz@gmail.com>
2024-07-24 00:03:13 +0800

    fix: webapp

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -1,5 +1,15 @@
 import WebApp from '@twa-dev/sdk';
 
+declare global {
+    interface Window {
+        Telegram: {
+            WebApp: typeof WebApp;
+        };
+    }
+}
+
+export {};
+
 export const initializeTelegram = () => {
     if (window.Telegram && window.Telegram.WebApp) {
         WebApp.ready();
@@ -8,6 +18,10 @@ export const initializeTelegram = () => {
         console.error('Telegram WebApp is not available');
     }
 };
-export const getTelegramWebApp = () => {
-    return WebApp;
+
+export const getTelegramWebApp = (): typeof WebApp => {
+    if (!window.Telegram || !window.Telegram.WebApp) {
+        throw new Error('Telegram WebApp is not available');
+    }
+    return window.Telegram.WebApp;
 };
